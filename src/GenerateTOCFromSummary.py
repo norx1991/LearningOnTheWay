6f7c82e09825ee2e751ad1a808ff561b7ff3e492
norx1991 <63565283+SlowOneFly@users.noreply.github.com>
2022-09-01 21:20:13 -0500

    Minor change

--- src/GenerateTOCFromSummary.py
+++ src/GenerateTOCFromSummary.py
@@ -5,6 +5,6 @@ with open("SUMMARY.md", 'r') as summary:
     lines = summary.readlines()
     with open("toc.md", 'w') as toc:
         for line in lines:
-            if line.startswith(r'      '):  # Only include up to Level 2 title
+            if line.startswith(r'      '):  # Only include up to Level-2 title
                 continue
             toc.write(line)

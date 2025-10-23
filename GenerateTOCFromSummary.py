239c1b1c369dd32ec7b64603d8936763f2bc5509
Deploy from CI <>
2025-10-23 14:36:36 +0000

    Deploy 9bdbf21c218a306047724a41fa884d9ce4efb5c5 to gh-pages

---
+++ GenerateTOCFromSummary.py
@@ -0,0 +1,13 @@
+import os
+
+os.chdir("./src/")
+
+with open("SUMMARY.md", 'r') as summary:
+    lines = summary.readlines()
+    with open("toc.md", 'w') as toc:
+        for line in lines:
+            if line.startswith(r'    '):  # Only include up to Level-2 title
+                continue
+            if '/README.md)' in line:
+                line = line.split('[')[1].split(']')[0] + '\n'
+            toc.write(line)

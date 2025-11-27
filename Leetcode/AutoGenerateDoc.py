8b5bc146cb681930d1eb4d1e02d0efa777275707
Deploy from CI <>
2025-11-27 05:15:09 +0000

    Deploy 07d2eab5a3cca3886a7e12a13a3bb18171ee6950 to gh-pages

---
+++ Leetcode/AutoGenerateDoc.py
@@ -0,0 +1,23 @@
+import os
+from turtle import ScrolledCanvas
+
+questions = []
+
+os.chdir("./src/Leetcode")
+
+d = {}
+for line in open("question-list.md").readlines():
+    number = int(line.split('.')[0].split('[')[1])
+    title = line.split('[')[1].split(']')[0]
+    link = line.split('(')[1].split(')')[0]
+    d[number] = (title, link)
+
+for question in questions:
+    if os.path.isfile(str(question) + '.md'):
+        continue
+    fout = open(str(question) + '.md', 'w')
+    fout.write(f'[{d[question][0]}]({d[question][1]})')
+    fout.close()
+
+for question in sorted(questions):
+    print(f'[{d[question][0]}](Leetcode/{question}.md)')
